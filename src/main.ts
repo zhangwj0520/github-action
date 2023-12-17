f5e2ecae3540431b177ee153e78adef39a6cbaa9
张为杰 <zhangwj0520@hotmail.com>
2023-12-17 10:06:44 +0800

    remove jest

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ export async function run(): Promise<void> {
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core?.setFailed(error.message)
   }
 }
